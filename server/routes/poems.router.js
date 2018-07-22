b24d9574052a86c475a73aed1f6b9fe13ea42350
Zack Stout <zackstout@gmail.com>
2018-07-22 01:02:23 -0500

    ahhh yes the problem is async

--- server/routes/poems.router.js
+++ server/routes/poems.router.js
@@ -11,6 +11,8 @@ var config = {
   idleTimeoutMillis: 30000
 };
 
+const RANDOM_LENGTH = 10;
+
 var pool = new pg.Pool(config);
 console.log('router in');
 
@@ -49,16 +51,33 @@ router.get('/random', function (req, res) {
         if (err) {
           console.log(err);
         } else {
-          var queryText = `SELECT line, author, title, lineno FROM lines JOIN poems ON poems.id=lines.poem_id;`;
-          db.query(queryText, [], function (errorMakingQuery, result) {
-            done();
-            if (errorMakingQuery) {
-              console.log('Error with poems GET', errorMakingQuery);
-              res.sendStatus(501);
-            } else {
-              res.send(result.rows);
-            }
-          });
+          let random_indices = [];
+          for (let i=0; i < RANDOM_LENGTH; i++) {
+              // It's so many I won't even worry about possibility of duplicates for now:
+              random_indices.push(Math.floor(Math.random() * 422920));
+          }
+          console.log("indices: ", random_indices);
+
+          let resultRows = [];
+          while (random_indices.length > 0) {
+            const popped = random_indices.pop();
+            console.log(popped);
+            var queryText = `SELECT line_id, line, author, title, lineno FROM lines JOIN poems ON poems.poem_id=lines.poem_id WHERE line_id = ${popped};`;
+            db.query(queryText, [], function (errorMakingQuery, result) {
+              if (errorMakingQuery) {
+                console.log('Error with poems GET', errorMakingQuery);
+                res.sendStatus(501);
+              } else {
+                console.log(result);
+                resultRows.push(result.rows[0]);
+              }
+            });
+          }
+
+          // Oooh it must be some async problem...Duh.
+          console.log("rows: ", resultRows);
+          done();
+          res.send(resultRows);
         }
       });
 }); 
@@ -69,7 +88,7 @@ router.get('/term/:term', function (req, res) {
         if (err) {
           console.log(err);
         } else {
-          var queryText = `SELECT line, author, title, lineno FROM lines JOIN poems ON poems.id=lines.poem_id WHERE line LIKE '% ${req.params.term}%';`;
+          var queryText = `SELECT line, author, title, lineno FROM lines JOIN poems ON poems.poem_id=lines.poem_id WHERE line LIKE '% ${req.params.term}%';`;
           db.query(queryText, [], function (errorMakingQuery, result) {
             done();
             if (errorMakingQuery) {
